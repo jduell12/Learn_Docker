396a6042d23e12ada775017095ce49f39ac382c6
Jessica <duellje@oregonstate.edu>
2021-03-16 16:13:01 -0400

    restarting docker learning setup

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-const { isMainThread } = require('worker_threads');
-descripe('Array', function(){
-    describe('#indexOf()', function(){
-        isMainThread('should return -1 when the value is not present', function(){
-            assert.equal([1, 2, 3].indexOf(4), -1))
-        }
-    })
-})
